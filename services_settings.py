9a090275c6d55df73b22ad3e2a8e005fc6b5b21e
sethc23 <sethc23@users.noreply.github.com>
2015-09-16 19:42:35 -0400

    saving local content

---
+++ services_settings.py
@@ -0,0 +1,5 @@
+
+YELP_CONSUMER_KEY                           =   'QzH1O3EktEQt89kegeaUxQ'
+YELP_CONSUMER_SECRET                        =   'Q_GtZiKGtRvqQSTjGOSgsuogkTE'
+YELP_TOKEN                                  =   '6T7dDEQhf41FA2DIajIDZxsm4RwvBdM9'
+YELP_TOKEN_SECRET                           =   'YNaWq3LCjlQu9ir4Ibo-Zp6ELKI'
